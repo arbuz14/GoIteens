159cea8d56e41a14595d80fac650ee18dfd6bc7f
arbuz14 <pupil626@kg95.dp.ua>
2024-09-29 10:12:59 +0300

    Update film.py

--- telegram_bot/project/project/app/fsm/film.py
+++ telegram_bot/project/project/app/fsm/film.py
@@ -1,8 +1,4 @@
 from aiogram.fsm.state import State, StatesGroup
 
-class FilmCreateForm(StatesGroup):
-    title = State()
-    desc = State()
-    url = State()
-    photo = State()
-    rating = State()
+class ReviewForm(StatesGroup):
+    text = State()
